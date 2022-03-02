d41ba957972e243ad0a8d9daac39494d07d879fd
梦幻星空 <duhaijian@139.com>
2022-03-02 14:41:46 +0800

    Add files via upload

---
+++ Docker.sh
@@ -0,0 +1,6 @@
+#Debian / Ubuntu 卸载 Docker 教程
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+#一键安装 Docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
---
+++ ShellClash.sh
@@ -0,0 +1,1 @@
+sh -c "$(curl -kfsSl https://cdn.jsdelivr.net/gh/juewuy/ShellClash@master/install.sh)" && source /etc/profile &> /dev/null
